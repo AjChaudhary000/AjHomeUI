4ca4f6a853d399a18beb8c2000cc91b1a956d3e3
arjun chaudhary <arjun910661@gmail.com>
2021-06-22 14:04:45 +0530

    aj

--- HomeUI/HomeUI/RegPage4.swift
+++ HomeUI/HomeUI/RegPage4.swift
@@ -34,6 +34,26 @@ class RegPage4: UIViewController {
         label2.font = UIFont.boldSystemFont(ofSize:  14)
         return label2
     }()
+    private let  myimg:UIImageView   = {
+        let img = UIImageView(image:UIImage( named: "user"))
+        img.contentMode = .scaleAspectFill
+        img.layer.cornerRadius = 100
+        img.clipsToBounds = true
+        return img
+    }()
+    
+    private let btn1:UIButton = {
+        let bt = UIButton()
+        bt.setTitle("Please Select Your Profile ", for: .normal)
+        bt.addTarget(self, action: #selector(imagechecker), for: .touchUpInside)
+        return bt
+    }()
+    private let imagePicker:UIImagePickerController = {
+        let imagePicker = UIImagePickerController()
+        imagePicker.allowsEditing = false
+        return imagePicker
+    }()
+
     private let mySwitch:UISwitch = {
         let switcher = UISwitch()
        // switcher.addTarget(self, action: #selector(swOnOff), for: .valueChanged)
@@ -52,6 +72,9 @@ class RegPage4: UIViewController {
             view.layer.backgroundColor = UIColor(red:0.449 ,green: 0.229 ,blue: 0.808 ,alpha: 1).cgColor
         view.addSubview(myprogress)
         view.addSubview(mylabel1)
+        view.addSubview(myimg)
+        view.addSubview(btn1)
+        imagePicker.delegate = self
         view.addSubview(mylabel2)
         view.addSubview(mySwitch)
         view.addSubview(btn)
@@ -61,8 +84,10 @@ class RegPage4: UIViewController {
         
         myprogress.frame = CGRect(x: 15, y: 132, width: 313, height: 15)
         mylabel1.frame = CGRect(x: 97 , y: 172, width: 260, height: 23)
-        mylabel2.frame = CGRect(x: 22 , y: 235, width:330, height: 21)
-        mySwitch.frame = CGRect(x: 22 , y: 275, width:69, height: 30)
+        btn1.frame = CGRect(x: 45  , y:445, width: 261, height: 23)
+        myimg.frame = CGRect(x: 75  , y:529, width: 200, height: 200)
+        mylabel2.frame = CGRect(x: 22 , y: 493, width:330, height: 21)
+        mySwitch.frame = CGRect(x: 22 , y:539, width:69, height: 30)
         btn.frame = CGRect(x: 165  , y:577, width: 174, height: 58)
     }
     @objc func nextpage(){
@@ -71,4 +96,27 @@ class RegPage4: UIViewController {
         print("hello")
     }
     
+    @objc private func imagechecker() {
+        print("gallery called")
+        imagePicker.sourceType = .photoLibrary
+        DispatchQueue.main.async {
+            self.present(self.imagePicker, animated: true)
+        }
+    }
+
 }
+extension RegPage4:UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let selectedImage = info[.originalImage] as? UIImage {
+            myimg.image = selectedImage
+        }
+        
+        picker.dismiss(animated: true)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true)
+    }
+}
+
